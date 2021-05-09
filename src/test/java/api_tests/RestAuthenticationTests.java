8a50aac7720bbf6602d7cd17d1e25517ea641988
meher28 <mehermeka@gmail.com>
2021-05-09 16:05:40 +0100

    end to end scenario added 001

--- src/test/java/api_tests/RestAuthenticationTests.java
+++ src/test/java/api_tests/RestAuthenticationTests.java
@@ -34,7 +34,7 @@ public class RestAuthenticationTests {
     public void BasicAuthenticationTest() {
         baseURI = "https://bookstore.toolsqa.com";
         Map<String, String> _userCredentials = new HashMap<>();
-        _userCredentials.put("userName", "Meher78");
+        _userCredentials.put("userName", "Meher78757");
         _userCredentials.put("password", "Jaibaba01!");
 
         Gson _user = new Gson();
@@ -50,22 +50,114 @@ public class RestAuthenticationTests {
         System.out.println("User creation status " + _userCreationReponseDto);
         System.out.println("User Id created " + _userBody.userID + " : " + _userBody.username);
 
+/**
+ * Now that we created user lets get the user
+ */
+        String _getUserURI = "/Account/v1/User/{UUID}";
+        Response _getUserRes =
+                _reqSpec
+                        .pathParam("UUID", _userBody.userID)
+                        .get(_getUserURI);
 
-        //delete created user to keep the db tidy
+        UserDto _createdUser = _getUserRes.as(UserDto.class);
+        MessageDto _userReponseDto = _response.as(MessageDto.class);
+        System.out.println("User get status " + _userReponseDto);
+        System.out.println("Created User details: " + _createdUser);
+
+
+/*        //delete created user to keep the db tidy
         String _rawUserCredentials = _userCredentials.get("userName") + ":" + _userCredentials.get("password");
         byte[] _encodeToB64 = Base64.getEncoder().encode(_rawUserCredentials.getBytes());
         String _encodeCredentialsToBas64 = _encodeToB64.toString();
 
-        String _deletionPath = "/Account/v1/User/" + _userBody.userID;
+        String _deletionPath = "/Account/v1/User/{userId}";
 
         Response _deletionresponse =
-                _reqSpec.header("Authorization", "Basic " + _encodeCredentialsToBas64)
+                _reqSpec
+                        .pathParam("userId", _userBody.userID)
+                        .header("Authorization", "Basic " + _encodeCredentialsToBas64)
                         .delete(_deletionPath);
 
         MessageDto _deleteMessageBody = _deletionresponse.as(MessageDto.class);
 
-        System.out.println("Deletion Message Response :: " + _deleteMessageBody);
+        System.out.println("Deletion Message Response :: " + _deleteMessageBody);*/
 
     }
 
+    @Test(testName = "Bearer Authentication Test")
+    public void BearerAuthenticationTest() {
+        baseURI = "https://bookstore.toolsqa.com";
+        String userCreateURI = "/Account/v1/User";
+        String generateTokenURI = "/Account/v1/GenerateToken";
+        String addBookURI = "/BookStore/v1/Books";
+        String getUserDetailsURI = "/Account/v1/User/{UUID}";
+
+        Map<String, String> _userCredentials = new HashMap<>();
+        _userCredentials.put("userName", "Meher106");
+        _userCredentials.put("password", "Jaibaba01!");
+
+        Gson _user = new Gson();
+        RequestSpecification _reqSpec = given();
+        String _convert = _user.toJson(_userCredentials);
+
+
+        //Create user for user basic authentication.
+        Response _userCreationResponse = given()
+                .contentType(ContentType.JSON)
+                .body(_convert)
+                .post(userCreateURI);
+        UserDto _userBody = _userCreationResponse.body().as(UserDto.class);
+        MessageDto _userCreationResponseDto = _userCreationResponse.as(MessageDto.class);
+        System.out.println("User creation status " + _userCreationResponseDto);
+        System.out.println("User Id created " + _userBody.userID + " : " + _userBody.username);
+
+        //Generate token for the above createdUserAccount
+        Response _tokenGenerationResponse =
+                given()
+                        .body(_convert)
+                        .contentType(ContentType.JSON)
+                        .post(generateTokenURI);
+
+        MessageDto _generateTokenMessage = _tokenGenerationResponse.as(MessageDto.class);
+        System.out.println(_generateTokenMessage);
+
+        //now do a put to insert book for the user account
+        String _addBookPayload = "{\n" +
+                "  \"userId\": \"" + _userBody.userID + "\",\n" +
+                "  \"collectionOfIsbns\": [\n" +
+                "    {\n" +
+                "      \"isbn\": \"9781449331818\"\n" +
+                "    }\n" +
+                "  ]\n" +
+                "}";
+
+        Response _addBookToUserAccount =
+                given()
+                        .contentType(ContentType.JSON)
+                        .body(_addBookPayload)
+                        .header("Authorization", "Bearer " + _generateTokenMessage.token)
+                        .post(addBookURI);
+
+        MessageDto _bookAdditionDto = _addBookToUserAccount.as(MessageDto.class);
+
+        System.out.println("Book addition Status " + _bookAdditionDto);
+
+        //get user details
+
+        Response _getUserDetailsResponse =
+                given()
+                        .header("Authorization", "Bearer " + _generateTokenMessage.token)
+                        .pathParam("UUID", _userBody.userID)
+                        .get(getUserDetailsURI);
+
+        MessageDto _getUserMessageResponse = _getUserDetailsResponse.as(MessageDto.class);
+
+        System.out.println("Get user details message dto : " + _getUserMessageResponse);
+        UserDto _updatedUser = _getUserDetailsResponse.getBody().as(UserDto.class);
+
+        System.out.println("Updated User Details " + _updatedUser);
+
+        //use put request to update existing book
+
+    }
 }
--- src/test/java/dtos/MessageDto.java
+++ src/test/java/dtos/MessageDto.java
@@ -3,12 +3,20 @@ package dtos;
 public class MessageDto {
     public String code;
     public String message;
+    public String token;
+    public String expires;
+    public String status;
+    public String result;
 
     @Override
     public String toString() {
         return "MessageDto{" +
                 "code='" + code + '\'' +
                 ", message='" + message + '\'' +
+                ", token='" + token + '\'' +
+                ", expires='" + expires + '\'' +
+                ", status='" + status + '\'' +
+                ", result='" + result + '\'' +
                 '}';
     }
 }
